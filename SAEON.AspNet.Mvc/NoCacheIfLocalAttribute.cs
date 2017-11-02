a9c567d7b7791caf02d1e8323bb8efc2132c3e93
Tim Parker-Nance <timpn@saeon.ac.za>
2017-11-02 16:21:18 +0200

    Added SAEON.AspNet.Mvc

---
+++ SAEON.AspNet.Mvc/NoCacheIfLocalAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Mvc; 
+
+namespace SAEON.AspNet.Mvc
+{ 
+    public class NoCacheIfLocalAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsLocal)
+            {
+                filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            }
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
