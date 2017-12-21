7ae9886f602287c28374ee996bf3dc5947564e30
Tim Parker-Nance <timpn@saeon.ac.za>
2017-12-21 15:33:09 +0200

    SAEON.OpenXML added

--- SAEON.Core/Extensions/StringExtensions.cs
+++ SAEON.Core/Extensions/StringExtensions.cs
@@ -1,14 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SAEON.Core
 {
     public static class StringExtensions  
     {
+
+        public static string DoubleQuoted(this string source)
+        {
+            return source.Quoted('"'); 
+        } 
+
+        public static string Quoted(this string source, char quote)
+        {
+            return
+                quote + source.Replace(Convert.ToString(quote), Convert.ToString(quote) + Convert.ToString(quote)) + quote;
+        }
+
         public static string Replace(this string source, Dictionary<string, string> dictionary)
         {
             string result = source;
             foreach (var kv in dictionary) result = result.Replace(kv.Key, kv.Value);
-            return result;
+            return result; 
+        }
+
+        public static string SingleQuoted(this string source)
+        {
+            return source.Quoted('\'');
         }
     } 
 } 
