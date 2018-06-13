5c9bb5f76c33f72389c623a074d01a6d94f4ac14
Tim Parker-Nance <timpn@saeon.ac.za>
2018-06-13 10:15:01 +0200

    Version bump

--- SAEON.Logs/Logging.cs
+++ SAEON.Logs/Logging.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 #endif  
 using Serilog;
 using Serilog.Context;
-using System; 
+using System;  
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
