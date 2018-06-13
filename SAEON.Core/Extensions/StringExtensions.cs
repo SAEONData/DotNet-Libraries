24a60d4a6bd79ec4ece624b1f20a12679aa8b592
Tim Parker-Nance <timpn@saeon.ac.za>
2018-06-13 10:14:29 +0200

    TrimEnd added

--- SAEON.Core/Extensions/StringExtensions.cs
+++ SAEON.Core/Extensions/StringExtensions.cs
@@ -3,30 +3,36 @@ using System.Collections.Generic;
 
 namespace SAEON.Core
 {
-    public static class StringExtensions  
+    public static class StringExtensions
     {
 
         public static string DoubleQuoted(this string source)
-        {
-            return source.Quoted('"'); 
-        } 
+        { 
+            return source.Quoted('"');
+        }
 
         public static string Quoted(this string source, char quote)
         {
             return
                 quote + source.Replace(Convert.ToString(quote), Convert.ToString(quote) + Convert.ToString(quote)) + quote;
         }
-         
+
+
         public static string Replace(this string source, Dictionary<string, string> dictionary)
         {
             string result = source;
             foreach (var kv in dictionary) result = result.Replace(kv.Key, kv.Value);
-            return result; 
+            return result;
         }
 
         public static string SingleQuoted(this string source)
         {
             return source.Quoted('\'');
         }
-    } 
-} 
+
+        public static string TrimEnd(this string source, string suffix)
+        {
+            return !source.EndsWith(suffix) ? suffix : source.Remove(source.Length - suffix.Length);
+        }
+    }
+}
