7dd853820fa9d73273d4b58d219d8b3f95c33687
Tim Parker-Nance <timpn@saeon.ac.za>
2019-05-03 10:31:04 +0200

    SAEON.AspNet.Common added

---
+++ SAEON.AspNet.Common/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace SAEON.AspNet.Common
+{
+    public static class Constants
+    {
+        public static readonly string AcceptEncoding = "Accept-Encoding";
+        public static readonly string ApplicationJson = "application/json";
+        public static readonly string ClaimClientId = "client_id";
+        public static readonly string ClaimRole = "role";
+        public static readonly string ContentEncoding = "Content-Encoding";
+        public static readonly string ContentSecurityPolicy = "ContentSecurityPolicy";
+        public static readonly string DefaultTenant = "DefaultTenant";
+        public static readonly string TenantHeaderId = "x-data-tenant";
+        public static readonly string Tenants = "Tenants";
+    }
+}
