1ccd306ea685d6f4ca4822613f07ce5bd1afdcf4
Tim Parker-Nance <timpn@saeon.ac.za>
2019-10-15 14:03:50 +0200

    .NetCore 3.0

--- SAEON.Core.WPF/Helpers/DesignerHelper.cs
+++ SAEON.Core.WPF/Helpers/DesignerHelper.cs
@@ -3,9 +3,8 @@ using System.Windows;
 
 namespace SAEON.Core
 {
-    public static class DesignerHelper 
+    public static class DesignerHelper
     {
         public static bool IsInDesignMode { get { return DesignerProperties.GetIsInDesignMode(new DependencyObject()) || (LicenseManager.UsageMode == LicenseUsageMode.Designtime); } }
     }
 }
- 
--- SAEON.Core/Extensions/EpochExtensions.cs
+++ SAEON.Core/Extensions/EpochExtensions.cs
@@ -4,13 +4,13 @@ namespace SAEON.Core.Extensions
 {
     public static class EpochExtensions
     {
-        public static DateTime FromEpoch(this int unixTime) 
+        public static DateTime FromEpoch(this int unixTime)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return epoch.AddSeconds(unixTime);
         }
 
-        public static int ToEpoch(this DateTime date) 
+        public static int ToEpoch(this DateTime date)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return Convert.ToInt32((date.ToUniversalTime() - epoch).TotalSeconds);
