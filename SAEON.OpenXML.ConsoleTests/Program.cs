8eb54292c5224899c9d9d6c5a3d118288f927f10
Tim Parker-Nance (SAEON) <timpn@saeon.ac.za>
2020-10-07 12:33:56 +0200

    ExcelSaxHelper DateTimeOffset, Enums added

--- SAEON.OpenXML.ConsoleTests/Program.cs
+++ SAEON.OpenXML.ConsoleTests/Program.cs
@@ -1,4 +1,7 @@
-﻿using SAEON.Logs;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using SAEON.Logs;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -56,10 +59,11 @@ namespace SAEON.OpenXML.ConsoleTests
                     SAEONLogs.Information("Deleting spreadsheet if exists");
                     if (File.Exists("LargeFile.xlsx")) File.Delete("LargeFile.xlsx");
                     SAEONLogs.Information("Creating spreadsheet");
-                    using (ExcelSaxHelper.CreateSpreadsheet("LargeFile.xlsx", CreateList()))
-                    {
+                    //using (ExcelSaxHelper.CreateSpreadsheet("LargeFile.xlsx", CreateList()))
+                    //{
 
-                    }
+                    //}
+                    CreateSpreadsheet();
                     SAEONLogs.Information("Done");
 
                 }
@@ -76,6 +80,86 @@ namespace SAEON.OpenXML.ConsoleTests
                 result.Add(new Test { ANumber = 1, AString = "Test" });
                 return result;
             }
+
+            void CreateSpreadsheet()
+            {
+                using (SpreadsheetDocument xl = SpreadsheetDocument.Create("LargeFile.xlsx", SpreadsheetDocumentType.Workbook))
+                {
+                    List<OpenXmlAttribute> oxa;
+                    OpenXmlWriter oxw;
+
+                    xl.AddWorkbookPart();
+                    WorksheetPart wsp = xl.WorkbookPart.AddNewPart<WorksheetPart>();
+
+                    oxw = OpenXmlWriter.Create(wsp);
+                    oxw.WriteStartElement(new Worksheet());
+                    oxw.WriteStartElement(new SheetData());
+
+                    //for (int i = 1; i <= 50000; ++i)
+                    for (int i = 1; i <= 50; ++i)
+                    {
+                        oxa = new List<OpenXmlAttribute>();
+                        // this is the row index
+                        oxa.Add(new OpenXmlAttribute("r", null, i.ToString()));
+
+                        oxw.WriteStartElement(new Row(), oxa);
+
+                        //for (int j = 1; j <= 100; ++j)
+                        for (int j = 1; j <= 10; ++j)
+                        {
+                            oxa = new List<OpenXmlAttribute>();
+                            // this is the data type ("t"), with CellValues.String ("str")
+                            oxa.Add(new OpenXmlAttribute("t", null, "str"));
+
+                            // it's suggested you also have the cell reference, but
+                            // you'll have to calculate the correct cell reference yourself.
+                            // Here's an example:
+                            //oxa.Add(new OpenXmlAttribute("r", null, "A1"));
+
+                            oxw.WriteStartElement(new Cell(), oxa);
+
+                            oxw.WriteElement(new CellValue(string.Format("R{0}C{1}", i, j)));
+
+                            // this is for Cell
+                            oxw.WriteEndElement();
+                        }
+
+                        // this is for Row
+                        oxw.WriteEndElement();
+                    }
+
+                    // this is for SheetData
+                    oxw.WriteEndElement();
+                    // this is for Worksheet
+                    oxw.WriteEndElement();
+                    oxw.Close();
+
+                    oxw = OpenXmlWriter.Create(xl.WorkbookPart);
+                    oxw.WriteStartElement(new Workbook());
+                    oxw.WriteStartElement(new Sheets());
+
+                    // you can use object initialisers like this only when the properties
+                    // are actual properties. SDK classes sometimes have property-like properties
+                    // but are actually classes. For example, the Cell class has the CellValue
+                    // "property" but is actually a child class internally.
+                    // If the properties correspond to actual XML attributes, then you're fine.
+                    oxw.WriteElement(new Sheet()
+                    {
+                        Name = "Sheet1",
+                        SheetId = 1,
+                        Id = xl.WorkbookPart.GetIdOfPart(wsp)
+                    });
+
+                    // this is for Sheets
+                    oxw.WriteEndElement();
+                    // this is for Workbook
+                    oxw.WriteEndElement();
+                    oxw.Close();
+
+                    xl.Close();
+                }
+            }
+
         }
 
     }
--- SAEON.OpenXML/ExcelSaxHelper.cs
+++ SAEON.OpenXML/ExcelSaxHelper.cs
@@ -5,6 +5,7 @@ using SAEON.Core;
 using SAEON.Logs;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
@@ -18,32 +19,22 @@ namespace SAEON.OpenXML
         private static Dictionary<string, int> sharedStrings = new Dictionary<string, int>();
         private static int sharedStringsIndex;
 
-        private static void WriteCellValue(OpenXmlWriter writer, object value, List<OpenXmlAttribute> attributes = null)
+        private static void WriteCellValue(OpenXmlWriter writer, object value, int row, int col)
         {
+            if (value == null) return;
+            var attributes = new List<OpenXmlAttribute>
+            {
+                new OpenXmlAttribute("r", null, $"{ExcelHelper.GetColumnName(col)}{row}")
+            };
             if (value is string aString)
             {
-                if (attributes == null)
-                {
-                    attributes = new List<OpenXmlAttribute>();
-                }
-                if (!UseSharedStrings)
-                {
-                    attributes.Add(new OpenXmlAttribute("t", null, "inlineStr"));
-                    writer.WriteStartElement(new Cell(), attributes);
-                    writer.WriteElement(new InlineString(new Text(aString)));
-                    writer.WriteEndElement();
-                }
-                else
-                {
-                    attributes.Add(new OpenXmlAttribute("t", null, "s"));
-                    writer.WriteStartElement(new Cell(), attributes);
-                    if (!sharedStrings.ContainsKey(aString))
-                    {
-                        sharedStrings.Add(aString, sharedStringsIndex++);
-                    }
-                    writer.WriteElement(new CellValue(sharedStrings[aString].ToString()));
-                    writer.WriteEndElement();
-                }
+                WriteString(aString);
+            }
+            else if ((value is int) || value is long || (value is double) || (value is float) || (value is decimal))
+            {
+                writer.WriteStartElement(new Cell(), attributes);
+                writer.WriteElement(new CellValue(value.ToString()));
+                writer.WriteEndElement();
             }
             else if (((value is int?) && ((int?)value).HasValue) ||
                      ((value is long?) && ((long?)value).HasValue) ||
@@ -51,23 +42,12 @@ namespace SAEON.OpenXML
                      ((value is float?) && ((float?)value).HasValue) ||
                      ((value is decimal?) && ((decimal?)value).HasValue))
             {
-                if (attributes == null)
-                {
-                    writer.WriteStartElement(new Cell() { DataType = CellValues.Number });
-                }
-                else
-                {
-                    writer.WriteStartElement(new Cell() { DataType = CellValues.Number }, attributes);
-                }
+                writer.WriteStartElement(new Cell(), attributes);
                 writer.WriteElement(new CellValue(value.ToString()));
                 writer.WriteEndElement();
             }
             else if (value is bool aBool)
             {
-                if (attributes == null)
-                {
-                    attributes = new List<OpenXmlAttribute>();
-                }
                 attributes.Add(new OpenXmlAttribute("t", null, "b"));
                 writer.WriteStartElement(new Cell(), attributes);
                 writer.WriteElement(new CellValue(aBool ? "1" : "0"));
@@ -75,10 +55,6 @@ namespace SAEON.OpenXML
             }
             else if (value is DateTime aDateTime)
             {
-                if (attributes == null)
-                {
-                    attributes = new List<OpenXmlAttribute>();
-                }
                 attributes.Add(new OpenXmlAttribute("s", null, "1"));
                 writer.WriteStartElement(new Cell() { DataType = CellValues.Number }, attributes);
                 writer.WriteElement(new CellValue(aDateTime.ToOADate().ToString()));
@@ -86,28 +62,60 @@ namespace SAEON.OpenXML
             }
             else if ((value is DateTime?) && ((DateTime?)value).HasValue)
             {
-                if (attributes == null)
-                {
-                    attributes = new List<OpenXmlAttribute>();
-                }
                 attributes.Add(new OpenXmlAttribute("s", null, "1"));
                 writer.WriteStartElement(new Cell() { DataType = CellValues.Number }, attributes);
-                writer.WriteElement(new CellValue(((DateTime?)value).Value.ToOADate().ToString()));
+                writer.WriteElement(new CellValue($"{((DateTime?)value).Value.ToOADate()}"));
                 writer.WriteEndElement();
             }
-            else
+            else if (value is DateTimeOffset aDateTimeOffset)
             {
-                if (attributes == null)
+                attributes.Add(new OpenXmlAttribute("s", null, "1"));
+                writer.WriteStartElement(new Cell() { DataType = CellValues.Number }, attributes);
+                writer.WriteElement(new CellValue(aDateTimeOffset.UtcDateTime.ToOADate().ToString()));
+                writer.WriteEndElement();
+            }
+            else if ((value is DateTimeOffset?) && ((DateTimeOffset?)value).HasValue)
+            {
+                attributes.Add(new OpenXmlAttribute("s", null, "1"));
+                writer.WriteStartElement(new Cell() { DataType = CellValues.Number }, attributes);
+                writer.WriteElement(new CellValue($"{((DateTimeOffset?)value).Value.UtcDateTime.ToOADate()}"));
+                writer.WriteEndElement();
+            }
+            else if (value is Enum aEnum)
+            {
+                WriteString(aEnum.ToString());
+            }
+            else // Ignore unknown types
+            {
+                WriteString($"Unknown type {value.GetType().Name} {value}");
+            }
+
+            void WriteString(string aValue, bool forceInline = false)
+            {
+                if (!UseSharedStrings || forceInline)
                 {
-                    writer.WriteStartElement(new Cell() { DataType = CellValues.Number });
+                    attributes.Add(new OpenXmlAttribute("t", null, "inlineStr"));
+                    writer.WriteStartElement(new Cell(), attributes);
+                    writer.WriteElement(new InlineString(new Text(aValue)));
+                    writer.WriteEndElement();
                 }
                 else
                 {
-                    writer.WriteStartElement(new Cell() { DataType = CellValues.Number }, attributes);
+                    attributes.Add(new OpenXmlAttribute("t", null, "s"));
+                    writer.WriteStartElement(new Cell(), attributes);
+                    if (!sharedStrings.ContainsKey(aValue))
+                    {
+                        sharedStrings.Add(aValue, sharedStringsIndex++);
+                    }
+                    writer.WriteElement(new CellValue($"{sharedStrings[aValue]}"));
+                    writer.WriteEndElement();
                 }
-                writer.WriteElement(new CellValue(value.ToString()));
-                writer.WriteEndElement();
             }
+        }
+
+        private static void WriteRowStart(OpenXmlWriter writer, int row)
+        {
+            writer.WriteStartElement(new Row(), new List<OpenXmlAttribute> { new OpenXmlAttribute("r", null, row.ToString()) });
         }
 
         private static SpreadsheetDocument CreateSpreadsheetDocument<T>(SpreadsheetDocument document, List<T> list) where T : class
@@ -137,26 +145,29 @@ namespace SAEON.OpenXML
 
                     using (var writer = OpenXmlWriter.Create(worksheetPart))
                     {
-                        writer.WriteStartElement(new Worksheet());
-                        writer.WriteStartElement(new SheetData());
-
-                        // Header
-                        writer.WriteStartElement(new Row());
-                        var props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty);
-                        foreach (var prop in props)
-                        {
-                            WriteCellValue(writer, prop.Name);
-                        }
-                        writer.WriteEndElement(); //end of Row tag
-
                         var sw = new Stopwatch();
                         sw.Start();
                         var last = new TimeSpan();
                         var lastLog = new TimeSpan(last.Ticks);
+
+                        writer.WriteStartElement(new Worksheet());
+                        writer.WriteStartElement(new SheetData());
+
+                        var props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty);
+
                         int r = 1;
-                        int rMax = list.Count;
+                        int rMax = list.Count + 1;
+                        WriteRowStart(writer, r);
+                        int c = 1;
+                        foreach (var prop in props)
+                        {
+                            WriteCellValue(writer, prop.Name, r, c++);
+                        }
+                        writer.WriteEndElement(); //end of Row tag
+
                         foreach (var item in list)
                         {
+                            r++;
                             var rPerc = 1.0 * r / rMax;
                             var elapsed = sw.Elapsed;
                             var timeMax = new TimeSpan((long)(elapsed.Ticks / rPerc));
@@ -166,11 +177,117 @@ namespace SAEON.OpenXML
                                 lastLog = elapsed;
                             }
                             last = elapsed;
-                            r++;
-                            writer.WriteStartElement(new Row());
+
+                            WriteRowStart(writer, r);
+                            c = 1;
                             foreach (var prop in props)
                             {
-                                WriteCellValue(writer, prop.GetValue(item));
+                                WriteCellValue(writer, prop.GetValue(item), r, c++);
+                            }
+                            writer.WriteEndElement(); //end of Row tag
+                        }
+
+                        writer.WriteEndElement(); //end of SheetData
+                        writer.WriteEndElement(); //end of worksheet
+                        writer.Close();
+                        SAEONLogs.Information("Processed {0} rows in {1}", rMax, sw.Elapsed.TimeStr());
+                    }
+                    if (sharedStrings.Any())
+                    {
+                        var sharedStringPart = workbookPart.AddNewPart<SharedStringTablePart>();
+                        using (var writer = OpenXmlWriter.Create(sharedStringPart))
+                        {
+                            writer.WriteStartElement(new SharedStringTable());
+                            foreach (var item in sharedStrings)
+                            {
+                                writer.WriteStartElement(new SharedStringItem());
+                                writer.WriteElement(new Text(item.Key));
+                                writer.WriteEndElement();
+                            }
+                            writer.WriteEndElement();
+                        }
+                    }
+                    return document;
+                }
+                catch (Exception ex)
+                {
+                    SAEONLogs.Exception(ex);
+                    throw;
+                }
+            }
+        }
+
+        private static SpreadsheetDocument CreateSpreadsheetDocument(SpreadsheetDocument document, DataTable table)
+        {
+            using (SAEONLogs.MethodCall(typeof(SpreadsheetDocument)))
+            {
+                try
+                {
+                    // Add a WorkbookPart to the document.
+                    var workbookPart = document.AddWorkbookPart();
+
+                    // Add default styles
+                    var workbookStylesPart = workbookPart.AddNewPart<WorkbookStylesPart>();
+                    var style = workbookStylesPart.Stylesheet = ExcelHelper.CreateStylesheet();
+                    style.Save();
+
+                    var workbook = workbookPart.Workbook = new Workbook();
+                    var sheets = workbook.AppendChild<Sheets>(new Sheets());
+
+                    // create worksheet 1
+                    var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                    var sheet = new Sheet() { Id = workbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Sheet1" };
+                    sheets.Append(sheet);
+
+                    sharedStrings = new Dictionary<string, int>();
+                    sharedStringsIndex = 0;
+
+                    using (var writer = OpenXmlWriter.Create(worksheetPart))
+                    {
+                        var sw = new Stopwatch();
+                        sw.Start();
+                        var last = new TimeSpan();
+                        var lastLog = new TimeSpan(last.Ticks);
+
+                        writer.WriteStartElement(new Worksheet());
+                        writer.WriteStartElement(new SheetData());
+
+                        int r = 1;
+                        int rMax = table.Rows.Count + 1;
+                        WriteRowStart(writer, r);
+                        int c = 1;
+                        foreach (DataColumn column in table.Columns)
+                        {
+                            WriteCellValue(writer, column.ColumnName, r, c++);
+                        }
+                        writer.WriteEndElement(); //end of Row tag
+
+                        foreach (DataRow row in table.Rows)
+                        {
+                            r++;
+                            var rPerc = 1.0 * r / rMax;
+                            var elapsed = sw.Elapsed;
+                            var timeMax = new TimeSpan((long)(elapsed.Ticks / rPerc));
+                            if ((elapsed - lastLog).TotalSeconds >= 60)
+                            {
+                                SAEONLogs.Information("Row {0} of {1}, {2:F2}% complete, in {3}, {4} of {5}, {6} left", r, rMax, rPerc * 100.0, (elapsed - last).TimeStr(), elapsed.TimeStr(), timeMax, (timeMax - elapsed).TimeStr());
+                                lastLog = elapsed;
+                            }
+                            last = elapsed;
+
+                            WriteRowStart(writer, r);
+                            c = 1;
+                            foreach (DataColumn column in table.Columns)
+                            {
+                                if (row.IsNull(column))
+                                {
+                                    WriteCellValue(writer, null, r, c++);
+
+                                }
+                                else
+                                {
+                                    WriteCellValue(writer, row[column], r, c++);
+                                }
                             }
                             writer.WriteEndElement(); //end of Row tag
                         }
@@ -209,6 +326,12 @@ namespace SAEON.OpenXML
         {
             if (list == null) throw new ArgumentNullException(nameof(list));
             return CreateSpreadsheetDocument(SpreadsheetDocument.Create(fileName, SpreadsheetDocumentType.Workbook), list);
+        }
+
+        public static SpreadsheetDocument CreateSpreadsheet<T>(string fileName, DataTable table) where T : class
+        {
+            if (table == null) throw new ArgumentNullException(nameof(table));
+            return CreateSpreadsheetDocument(SpreadsheetDocument.Create(fileName, SpreadsheetDocumentType.Workbook), table);
         }
 
     }
